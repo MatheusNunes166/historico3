030520e764d85dbe4012405c064ec318303b6df9
MatheusNunes166 <matheus06nunes@hotmail.com>
2023-10-23 14:52:06 -0300

    commit inicial

---
+++ src/main/resources/static/routes.js
@@ -0,0 +1,29 @@
+function controleRotasGet(url){
+    switch(url){
+        case "/logout":
+            gerarSwal(url);
+            break;
+        case "/cadastro":
+            $.get(url,function(data){
+                $(".container").html(data);
+                $("#enviar").click(cadastrarUsuario);
+            });
+            break;
+        case "/cadastro/software":
+                    $.get(url,function(data){
+                        $(".container").html(data);
+                        $("#enviar").click(enviarCadastroSoftware);
+                    });
+                    break;
+        case "/edit/usuario":
+                            $.get(url,function(data){
+                                $(".container").html(data);
+                                $("#salvar").click(salvarPerfil);
+                            });
+                            break;
+        default:
+            $.get(url,function(data){
+                $(".container").html(data);
+            });
+    }
+}
